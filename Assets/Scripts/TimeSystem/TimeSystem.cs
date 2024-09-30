f1b81b2bcf90c461dc52d19e2d1d5618c700bf93
jilikuo <joaovitorbariri@hotmail.com>
2024-09-30 20:23:14 -0300

    added dayEndHour + making hour variables public in timeSystem

--- Assets/Scripts/TimeSystem/TimeSystem.cs
+++ Assets/Scripts/TimeSystem/TimeSystem.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class TimeSystem : MonoBehaviour
 {
-    [SerializeField] private float dayStartHour = 8f;
+    public float dayStartHour = 8f;
+    public float dayEndHour = 18f;
     [SerializeField] private float framesPerHour = 60f;
     public TimeObject currentTime;
     public BoolVariable isGamePaused;
