4e8426fb74dff2fd45694c47f5af7ebed4b24a11
jilikuo <joaovitorbariri@hotmail.com>
2024-10-13 21:26:44 -0300

    Replaced Prototype Movement System with Movement System based on Waypoints

---
+++ Assets/Scripts/Navigation/MovementScript.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    North,
+    South,
+    West,
+    East
+}
+
+public class MovementScript : MonoBehaviour
+{
+    public Waypoints hqWaypoint;
+
+    private GameObject playerObject;
+    private Transform playerTransform;
+    private Transform cameraTransform;
+    private Waypoints currentWaypoint;
+
+    void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject == null)
+        {
+            playerObject = this.gameObject;
+        }
+
+        playerTransform = playerObject.transform;
+
+        if (currentWaypoint == null)
+        {
+            if (hqWaypoint == null)
+            {
+                throw new System.Exception("No HQ Waypoint assigned to MovementScript.");
+            }
+            currentWaypoint = hqWaypoint;
+        }
+
+        playerTransform.position = new Vector3(currentWaypoint.position.x, 
+                                               currentWaypoint.position.y, 
+                                               currentWaypoint.position.z);
+    }
+
+    public void MovePlayer(Direction direction)
+    {
+        switch (direction) 
+        {
+            case Direction.North:
+                currentWaypoint = currentWaypoint.northWaypoint;
+                break;
+            case Direction.South:
+                currentWaypoint = currentWaypoint.southWaypoint;
+                break;
+            case Direction.West:
+                currentWaypoint = currentWaypoint.westWaypoint;
+                break;
+            case Direction.East:
+                currentWaypoint = currentWaypoint.eastWaypoint;
+                break;
+            default:
+                throw new System.ArgumentException("Invalid Direction received at MovePlayer method");
+        }
+
+        playerTransform.position = new Vector3(currentWaypoint.position.x,
+                                               currentWaypoint.position.y,
+                                               currentWaypoint.position.z);
+
+        playerTransform.rotation = Quaternion.Euler(currentWaypoint.rotation.x,
+                                                    currentWaypoint.rotation.y,
+                                                    currentWaypoint.rotation.z);
+    }
+
+    public bool ValidateDirection(Direction direction) { 
+        switch (direction)
+        {
+            case Direction.North:
+                return currentWaypoint.northWaypoint.isValid;
+            case Direction.South:
+                return currentWaypoint.southWaypoint.isValid;
+            case Direction.West:
+                return currentWaypoint.westWaypoint.isValid;
+            case Direction.East:
+                return currentWaypoint.eastWaypoint.isValid;
+            default:
+                throw new System.ArgumentException("Invalid Direction received at ValidateDirection method");
+        }
+    }
+
+    public bool CheckDirection(Direction direction) {
+        switch (direction)
+        {
+            case Direction.North:
+                return currentWaypoint.northWaypoint.isAccessible;
+            case Direction.South:
+                return currentWaypoint.southWaypoint.isAccessible;
+            case Direction.West:
+                return currentWaypoint.westWaypoint.isAccessible;
+            case Direction.East:
+                return currentWaypoint.eastWaypoint.isAccessible;
+            default:
+                throw new System.ArgumentException("Invalid Direction received at CheckDirection method");
+        }
+    }
+}
