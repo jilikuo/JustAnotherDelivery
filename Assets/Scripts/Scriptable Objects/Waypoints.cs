33bdda43b83de4bca6a9d08d46da4ce093aa5360
jilikuo <joaovitorbariri@hotmail.com>
2024-10-13 21:25:40 -0300

    Added the Waypoint System

---
+++ Assets/Scripts/Scriptable Objects/Waypoints.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+[Serializable]
+public class Waypoints : ScriptableObject
+{
+    [Serializable]
+    public struct Coordinates
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Coordinates(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    public StreetName streetName;
+    public int addressNumber;
+
+    public Coordinates position;
+    public Coordinates rotation;
+
+    public Waypoints northWaypoint;
+    public Waypoints eastWaypoint;
+    public Waypoints southWaypoint;
+    public Waypoints westWaypoint;
+
+    public bool isValid;
+    public bool isAccessible;
+}
