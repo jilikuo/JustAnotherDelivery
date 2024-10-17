f1e07607ad8350d05d9b818910d05b26ec9f7de1
Kelly Martines <chicsa@yahoo.com>
2024-10-16 20:05:21 -0500

    Moved TimeoutPanel logic to GameManager

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -40,6 +40,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         timeSystem = GameObject.FindGameObjectWithTag("TimeSystem").GetComponent<TimeSystem>();
+        timeSystem.timeoutEvent.AddListener(ShowTimeoutPanel);
 
 #if !UNITY_EDITOR
         if (SceneManager.GetActiveScene().buildIndex == 0)
@@ -47,6 +48,22 @@ public class GameManager : MonoBehaviour
             SceneManager.LoadScene(1);
         }
 #endif
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);
     }
 
     public void LoadNextScene()
@@ -103,5 +120,17 @@ public class GameManager : MonoBehaviour
     public void RewardForDelivery(Package package)
     {
         money++;
+    }
+
+    private void ShowTimeoutPanel()
+    {
+        var timeoutPanelParent = GameObject.FindGameObjectWithTag("TimeoutPanel");
+        if (timeoutPanelParent != null)
+        {
+            Debug.Log("Ran out of time");
+            // timeoutPanelParent is enabled to make it Findable.
+            // It has one child - the actual panel, which is disabled.
+            timeoutPanelParent.transform.GetChild(0).gameObject.SetActive(true);
+        }
     }
 }
