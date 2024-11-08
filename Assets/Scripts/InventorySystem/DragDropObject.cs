b41ef011e12168a5136dab502f6f84d2549f14d0
Mateus Ribeiro Culpo <mateusculpo@gmail.com>
2024-11-08 08:28:04 -0300

    feat: apply center the package in the cursor.

--- Assets/Scripts/InventorySystem/DragDropObject.cs
+++ Assets/Scripts/InventorySystem/DragDropObject.cs
@@ -17,6 +17,7 @@ public class DragDropObject : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
     protected Vector3 startPosition = Vector3.zero;
     protected IItemDraggable itemDraggableParent;
     protected Transform oldParent;
+    protected RectTransform rectTransform;
     protected int siblingIndex = -1;
 
     public Bounds GetWorldBounds()
@@ -34,8 +35,7 @@ public class DragDropObject : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        if (!eventData.button.Equals(PointerEventData.InputButton.Left))
-            return;
+        if (!eventData.button.Equals(PointerEventData.InputButton.Left)) return;
         //Debug.Log("OnBeginDrag: From: " + transform.position + " To: " + eventData.position);
         BeforeBeginDrag(eventData);
 
@@ -47,6 +47,22 @@ public class DragDropObject : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
         startPosition = transform.position;
         dragOffset = (Vector2)transform.position - eventData.position;
 
+        rectTransform = transform as RectTransform;
+
+        // Convert screen position to world position relative to the rectTransform
+        Vector3 globalMousePos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            rectTransform, eventData.position, eventData.pressEventCamera, out globalMousePos);
+
+        // Calculate the world size of the object considering its scale
+        Vector2 worldSizeObject = new Vector2(
+            rectTransform.sizeDelta.x * rectTransform.lossyScale.x,
+            rectTransform.sizeDelta.y * rectTransform.lossyScale.y
+        );
+
+        // Calculate the drag offset based on the pivot point
+        dragOffset = new Vector2((0.5f - rectTransform.pivot.x) * worldSizeObject.x, (0.5f - rectTransform.pivot.y) * worldSizeObject.y);
+
         // Transfer the object to the root, to keep it always on top
         itemDraggableParent = GetComponentInParent<IItemDraggable>();
         oldParent = transform.parent;
@@ -54,15 +70,26 @@ public class DragDropObject : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
         transform.SetParent(transform.GetComponent<RectTransform>().root, true);
         transform.SetAsLastSibling();
 
+        // Calculate the position of the object based on the global mouse position and the drag offset,
+        // updating the object's position to follow the mouse while considering its pivot point.
+        transform.position = globalMousePos - (Vector3)dragOffset;
+
+
         AfterBeginDrag(eventData);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (!eventData.button.Equals(PointerEventData.InputButton.Left))
-            return;
+        if (!eventData.button.Equals(PointerEventData.InputButton.Left)) return;
+
         //Debug.Log("OnDrag: From: " + transform.position + " To: " + eventData.position);
-        transform.position = eventData.position + dragOffset;
+
+        Vector3 globalMousePos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            rectTransform, eventData.position, eventData.pressEventCamera, out globalMousePos);
+
+        transform.position = globalMousePos - (Vector3)dragOffset;
+
     }
 
     protected virtual void BeforeEndDrag(PointerEventData eventData)
@@ -75,8 +102,9 @@ public class DragDropObject : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (!eventData.button.Equals(PointerEventData.InputButton.Left))
-            return;
+        if (!eventData.button.Equals(PointerEventData.InputButton.Left)) return;
+
+
         //Debug.Log("OnEndDrag: From: " + transform.position + " To: " + eventData.position);
         BeforeEndDrag(eventData);
 
@@ -84,7 +112,12 @@ public class DragDropObject : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
         var itemDroppableParent = (newParent != null) ? newParent.GetComponent<IItemDroppable>() : null;
         if (itemDroppableParent != null && itemDroppableParent.IsValidDropPosition(this))
         {
-            transform.position = eventData.position + dragOffset;
+            Vector3 globalMousePos;
+
+            RectTransformUtility.ScreenPointToWorldPointInRectangle(
+                transform as RectTransform, eventData.position, eventData.pressEventCamera, out globalMousePos);
+
+            transform.position = globalMousePos - (Vector3)dragOffset;
             if (itemDraggableParent != null)
             {
                 itemDraggableParent.RemoveDragDropObject(this);
