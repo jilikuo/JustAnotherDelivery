fe096ea19e0e1f9a04ce34ce309ad7eb7d8ab74b
Kelly Martines <chicsa@yahoo.com>
2024-11-27 21:34:37 -0600

    Added clearing of bin directory and zip

--- build_game.bat
+++ build_game.bat
@@ -2,4 +2,7 @@ set mypath=%cd%
 @echo %mypath%
 git checkout main
 git pull
+rmdir /s bin
+mkdir bin
 "C:\Program Files\Unity\Hub\Editor\2022.3.31f1\Editor\Unity.exe" -quit -batchmode -logFile build_game.log -projectPath %mypath% -executeMethod GameBuilder.Build
+for /d %%X in (bin\*) do "c:\Program Files\7-Zip\7z.exe" a -tzip "%%X.zip" "%%X\"
