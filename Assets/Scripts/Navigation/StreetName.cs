d0912d2682e8ae6c7ae5f1542dd9e3ae8f2812f8
jilikuo <joaovitorbariri@hotmail.com>
2024-12-02 17:34:21 -0300

    Updated street names

--- Assets/Scripts/Navigation/StreetName.cs
+++ Assets/Scripts/Navigation/StreetName.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 
 public enum Streets
 {
-    FirstAvenue, //A1-A13
-    SecondAvenue, //B2-B13
-    FirstStreet, //D1-D7
-    SecondStreet, //D10-D12
-    FirstLane, //C1-D1
-    SecondLane, //C4-D4
-    HQWay, //B10-D10
-    CentralStreet, //A7-D7 currently there are no buildings at this street
+    //Neighborhood 01
+    Lorryload,
+    Haulage,
+    Remittance,
+    Freightage,
+    Portage,
+    Batch
 }
 
 [Serializable]
@@ -24,26 +23,13 @@ public class StreetName
     {
         get 
         {
-            name = streetNames[streetId];
-            return name; 
+            return streetId.ToString(); 
         }
     }
-
-    private static Dictionary<Streets, string> streetNames = new Dictionary<Streets, string>
-    {
-        {Streets.FirstAvenue, "Completely Normal Avenue"},
-        {Streets.SecondAvenue, "This Is Actually Fine Avenue"},
-        {Streets.FirstStreet, "This Is Definitely A Street Street"},
-        {Streets.SecondStreet, "Another Okay Street"},
-        {Streets.FirstLane, "Why Come Here Lane"},
-        {Streets.SecondLane, "Not A Street Lane"},
-        {Streets.HQWay, "Far Far Away Way"},
-        {Streets.CentralStreet, "Central Street"}
-    };
 
     public StreetName(Streets streetId)
     {
         this.streetId = streetId;
-        this.name = streetNames[streetId];
+        this.name = streetId.ToString();
     }
 }
